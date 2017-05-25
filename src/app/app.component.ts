2bd9bd26c851b72f178fd51bc44bcbf86eea5e55
Kane Stapler <kanestapler@gmail.com>
2017-05-24 21:44:09 -0500

    Trying to make nba package work:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as NBA from 'nba';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    selector: 'my-app',
+    template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent implements OnInit {
+
+    ngOnInit(): void {
+
+        //let curry = NBA.findPlayer('Stephen Curry');
+    }
+
+    name = 'Angular';
+}
